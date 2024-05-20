d059c9bacb76ebb334b1ddc28d6c98d4ab60055c
BuildTools <unconfigured@null.spigotmc.org>
2024-05-20 11:36:09 +0200

    Actualización de la clase Colors a solo los colores que si se usaron finalmente

--- src/board/Colors.java
+++ src/board/Colors.java
@@ -17,28 +17,21 @@ public class Colors {
 
 	// RESET
 	public static final String RESET = "\u001B[0m";
+
 	// BACKGROUNDS
-	static final String WHITE_BG = "\u001B[47m";
-	static final String BLACK_BG = "\u001B[40m";
-	public static final String YELLOW_BG = "\u001B[43m";
-	static final String RED_BG = "\u001B[41m";
-	static final String LIGHT_YELLOW_BG = "\u001B[103m";
+	public static final String WHITE_BG = "\u001B[47m";
+	public static final String BLACK_BG = "\u001B[40m";
+	public static final String RED_BG = "\u001B[41m";
+	public static final String LIGHT_YELLOW_BG = "\u001B[103m";
 	public static final String LIGHT_GREEN_BG = "\u001B[102m";
+
 	// TEXTS
-	static final String BLACK_TEXT = "\u001B[30m";
-	static final String RED_TEXT = "\u001B[31m";
-	static final String GREEN_TEXT = "\u001B[32m";
-	static final String YELLOW_TEXT = "\u001B[33m";
-	static final String BLUE_TEXT = "\u001B[34m";
-	static final String PURPLE_TEXT = "\u001B[35m";
-	static final String CYAN_TEXT = "\u001B[36m";
-	static final String WHITE_TEXT = "\u001B[37m";
-	static final String DARK_GRAY_TEXT = "\u001B[90m";
-	static final String LIGHT_RED_TEXT = "\u001B[91m";
-	static final String LIGHT_GREEN_TEXT = "\u001B[92m";
-	static final String LIGHT_YELLOW_TEXT = "\u001B[93m";
-	static final String LIGHT_BLUE_TEXT = "\u001B[94m";
-	static final String LIGHT_PURPLE_TEXT = "\u001B[95m";
-	static final String LIGHT_CYAN_TEXT = "\u001B[96m";
-	static final String WHITE_TEXT_BRIGHT = "\u001B[97m";
+	public static final String BLACK_TEXT = "\u001B[30m";
+	public static final String RED_TEXT = "\u001B[31m";
+	public static final String GREEN_TEXT = "\u001B[32m";
+	public static final String YELLOW_TEXT = "\u001B[33m";
+	public static final String BLUE_TEXT = "\u001B[34m";
+	public static final String CYAN_TEXT = "\u001B[36m";
+	public static final String WHITE_TEXT = "\u001B[37m";
+	public static final String LIGHT_RED_TEXT = "\u001B[91m";
 }
